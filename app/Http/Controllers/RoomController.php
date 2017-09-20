e006ecc11aea78b1dc61ec948725bd33fd689eb2
nine <nine@segmentfault.com>
2017-09-20 19:44:45 +0800

    add

---
+++ app/Http/Controllers/RoomController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RoomController extends Controller
+{
+    //
+
+    public function index()
+    {
+        return view('welcome');
+    }
+}
---
+++ database/migrations/2017_09_20_054751_create_room_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRoomTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('room', function (Blueprint $table) {
+            $table->increments('id');
+            $table->mediumInteger('numbers');
+            $table->integer('owner');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('room');
+    }
+}
